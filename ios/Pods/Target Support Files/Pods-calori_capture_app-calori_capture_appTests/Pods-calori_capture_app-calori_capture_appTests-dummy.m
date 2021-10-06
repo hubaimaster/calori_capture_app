bc23716d9751a1990ea706847b02a2b909392a03
kimchanghwan <hubaimaster@gmail.com>
2021-10-06 22:00:10 +0900

    Initial commit

---
+++ ios/Pods/Target Support Files/Pods-calori_capture_app-calori_capture_appTests/Pods-calori_capture_app-calori_capture_appTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_calori_capture_app_calori_capture_appTests : NSObject
+@end
+@implementation PodsDummy_Pods_calori_capture_app_calori_capture_appTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-calori_capture_app/Pods-calori_capture_app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_calori_capture_app : NSObject
+@end
+@implementation PodsDummy_Pods_calori_capture_app
+@end
